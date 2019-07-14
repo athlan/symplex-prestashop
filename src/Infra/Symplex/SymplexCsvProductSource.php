0a6012097e4f7551c35c0ccc068bfdf4e44cda62
Athlan <athlan@vgroup.pl>
2019-07-14 15:23:10 +0200

    Support file encoding

--- src/Infra/Symplex/SymplexCsvProductSource.php
+++ src/Infra/Symplex/SymplexCsvProductSource.php
@@ -5,18 +5,28 @@ namespace Athlan\SymplexPrestahop\Infra\Symplex;
 use Iterator;
 use Athlan\SymplexPrestahop\Application\Product;
 use Athlan\SymplexPrestahop\Application\ProductsSource;
+use League\Csv\CharsetConverter;
 use League\Csv\Reader;
 
 class SymplexCsvProductSource implements ProductsSource
 {
+    const ENCODING_DEFAULT = 'iso-8859-2';
+
     /**
      * @var string
      */
     private $filePath;
 
-    public function __construct(string $filePath)
+    /**
+     * @var string
+     */
+    private $encoding;
+
+    public function __construct(string $filePath,
+                                string $encoding = self::ENCODING_DEFAULT)
     {
         $this->filePath = $filePath;
+        $this->encoding = $encoding;
     }
 
     /**
@@ -27,6 +37,9 @@ class SymplexCsvProductSource implements ProductsSource
         $csv = Reader::createFromPath($this->filePath, 'r');
         $csv->setDelimiter("\t");
         $csv->setHeaderOffset(0);
+
+        $encoder = (new CharsetConverter())->inputEncoding($this->encoding);
+        $csv = $encoder->convert($csv);
 
         foreach ($csv as $record) {
             yield $this->mapRecord($record);
